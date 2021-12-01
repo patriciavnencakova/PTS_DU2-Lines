41d258256cb2d40ef88f35f8e18d7c478f4f957e
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2021-12-01 20:48:01 +0100

    add Pair and Tuple

---
+++ src/connection/Pair.java
@@ -0,0 +1,11 @@
+package connection;
+
+public class Pair<T, S> {
+    private T I;
+    private S II;
+
+    public Pair(T I, S II) {
+        this.I = I;
+        this.II = II;
+    }
+}
---
+++ src/connection/Tuple.java
@@ -0,0 +1,13 @@
+package connection;
+
+public class Tuple<T, S, B> {
+    private T I;
+    private S II;
+    private B III;
+
+    public Tuple(T I, S II, B III) {
+        this.I = I;
+        this.II = II;
+        this.III = III;
+    }
+}
