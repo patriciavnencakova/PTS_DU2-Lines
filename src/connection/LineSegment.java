ed9abcd97a77e31ba8a1994893fcbbdc281a516c
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2021-12-02 21:28:47 +0100

    update LineSegment

--- src/connection/LineSegment.java
+++ src/connection/LineSegment.java
@@ -12,6 +12,13 @@ public class LineSegment implements LineSegmentInterface {
     private Integer capacity;
     private LineName lineName;
 
+    public LineSegment(Stop nextStop, TimeDiff timeToNextStop, HashMap<Time, Integer> numberOfPassengers, Integer capacity, LineName lineName) {
+        this.nextStop = nextStop;
+        this.timeToNextStop = timeToNextStop;
+        this.numberOfPassengers = numberOfPassengers;
+        this.capacity = capacity;
+        this.lineName = lineName;
+    }
 
     @Override
     public Pair<Time, StopName> nextStop(Time startTime) {
@@ -19,12 +26,17 @@ public class LineSegment implements LineSegmentInterface {
     }
 
     @Override
-    public Tuple<Time, StopName, Boolean> nextStopAndUpdateReachable(Time startTime) {
-        return null;
+    public Triplet<Time, StopName, Boolean> nextStopAndUpdateReachable(Time startTime) {
+        Time time = new Time(timeToNextStop.getTimeDiff() + startTime.getTime());
+        if (!(numberOfPassengers.get(startTime) < capacity)) return new Triplet<>(time, nextStop.getName(), false);
+        nextStop.updateReachableAt(time, lineName);
+        return new Triplet<>(time, nextStop.getName(), true);
     }
 
     @Override
-    public void incrementCapacity(Time startTime) {}
+    public void incrementCapacity(Time startTime) {
+        numberOfPassengers.put(startTime, numberOfPassengers.get(startTime) + 1);
+    }
 
     @Override
     public TimeDiff getTimeToNextStop() {
