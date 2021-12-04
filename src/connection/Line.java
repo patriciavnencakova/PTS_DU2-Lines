40e5e11d56c1658666cf1c4072dd3dfb8972f26d
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2021-12-04 17:39:30 +0100

    update Line

--- src/connection/Line.java
+++ src/connection/Line.java
@@ -20,23 +20,48 @@ public class Line implements LineInterface {
 
     @Override
     public void updateReachable(Time time, StopName stop) {
-        Time startingTime;
-        for (int i = 0; i < startingTimes.size(); i++) {
-            boolean exit = false;
-            startingTime = startingTimes.get(i);
-            Time currentTime = startingTime;
-            for (int j = 0; j < lineSegments.size(); j++) {
-                Pair<Time, StopName> nextStop = lineSegments.get(i).nextStop(currentTime);
-                if (nextStop.getII() == stop && nextStop.getI().getTime() >= time.getTime()) {
-                    exit = true;
-                    break;
-                }
-                currentTime = nextStop.getI();
-            }
-            if (exit) break;
+        Time lastPossible = startingTimes.get(startingTimes.size() - 1);
+        StopName currentStop = firstStop;
+        Time currentTime = lastPossible;
+        int indexOfStop = 0;
+        while (currentStop != stop) {
+            Pair<Time, StopName> nextStop = lineSegments.get(indexOfStop).nextStop(currentTime);
+            currentStop = nextStop.getII();
+            currentTime = nextStop.getI();
+            indexOfStop++;
+        }
+
+        if (currentTime.getTime() < time.getTime()) {
+            //TODO neexistuje spojenie pre tento cas
+        }
+
+        this.updateAllPossible(indexOfStop, currentTime);
+
+        int indexOfStartingTime = startingTimes.size() - 1;
+        TimeDiff length = new TimeDiff(currentTime.getTime() - lastPossible.getTime());
+        while (this.catchEarlier(time, length, indexOfStartingTime)) {
+            Time tmp = new Time(startingTimes.get(indexOfStartingTime - 1).getTime() + length.getTimeDiff());
+            this.updateAllPossible(indexOfStop, tmp);
+            indexOfStartingTime--;
         }
     }
 
     @Override
-    public void updateCapacityAndGetPreviousStop(StopName stop, Time time) {}
+    public StopName updateCapacityAndGetPreviousStop(StopName stop, Time time) {
+        return null;
+    }
+
+    public void updateAllPossible(int indexOfStop, Time currentTime) {
+        while (indexOfStop < lineSegments.size()) {
+            Triplet<Time, StopName, Boolean> nextStop = lineSegments.get(indexOfStop).nextStopAndUpdateReachable(currentTime);
+            currentTime = nextStop.getI();
+            if (!nextStop.getIII()) break;
+            indexOfStop++;
+        }
+    }
+
+    public Boolean catchEarlier(Time time, TimeDiff length, int index) {
+        if (index == 0) return false;
+        return (startingTimes.get(index - 1).getTime() + length.getTimeDiff()) >= time.getTime();
+    }
 }
