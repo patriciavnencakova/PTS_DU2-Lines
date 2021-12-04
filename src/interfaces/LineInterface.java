f2c82346c3504b34e5c24ca60fdd29408e218a65
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2021-12-04 17:09:45 +0100

    update LineInterface

--- src/interfaces/LineInterface.java
+++ src/interfaces/LineInterface.java
@@ -4,5 +4,5 @@ import datatypes.*;
 
 public interface LineInterface {
     void updateReachable(Time time, StopName stop);
-    void updateCapacityAndGetPreviousStop(StopName stop, Time time);
+    StopName updateCapacityAndGetPreviousStop(StopName stop, Time time);
 }
