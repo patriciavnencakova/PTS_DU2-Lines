ea4f5b25c268d68cb82ac6ace31d9b8170ba0b05
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2021-12-11 15:04:19 +0100

    Update InMemoryFactory

--- src/factories/InMemoryFactory.java
+++ src/factories/InMemoryFactory.java
@@ -1,30 +1,52 @@
 package factories;
 
-import datatypes.LineName;
-import datatypes.StopName;
-import inMemory.Stop;
-import interfaces.*;
+import datatypes.*;
+import connection.Line;
+import connection.LineSegment;
+import connection.Stop;
+import interfaces.FactoryInterface;
+import interfaces.LineInterface;
+import interfaces.StopInterface;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 
 public class InMemoryFactory implements FactoryInterface {
-    private HashMap<StopName, ArrayList<LineName>> inMemoryStops;
+    private final HashMap<StopName, ArrayList<LineName>> stops;
+    private final HashMap<LineName, Pair<ArrayList<Time>, StopName/*ArrayList<LineSegment>*/ >> lines;
+    private final HashMap<LineName, Quadruple<Stop, TimeDiff, HashMap<Time, Integer>, Integer>> lineSegments;
 
-    public InMemoryFactory(HashMap<StopName, ArrayList<LineName>> inMemoryStops) {
-        this.inMemoryStops = inMemoryStops;
+    public InMemoryFactory(
+            HashMap<StopName, ArrayList<LineName>> stops, HashMap<LineName,
+            Pair<ArrayList<Time>, StopName>> lines,
+            HashMap<LineName, Quadruple<Stop, TimeDiff, HashMap<Time, Integer>, Integer>> lineSegments) {
+        this.stops = new HashMap<>(stops);
+        this.lines = new HashMap<>(lines);
+        this.lineSegments = new HashMap<>(lineSegments);
     }
 
     @Override
     public Optional<StopInterface> createStop(StopName stopName) {
-        if (!inMemoryStops.containsKey(stopName)) return Optional.empty();
-        return Optional.of(new Stop(stopName, inMemoryStops.get(stopName)));
+        if (!stops.containsKey(stopName)) return Optional.empty();
+        return Optional.of(new Stop(stopName, stops.get(stopName)));
     }
 
     @Override
     public Optional<LineInterface> createLine(LineName lineName) {
-        return null;
+        if (!lines.containsKey(lineName)) return Optional.empty();
+        return Optional.of(new Line(lineName, lines.get(lineName).getI(), lines.get(lineName).getII(), getLineSegments(lineName)));
+    }
+
+    public ArrayList<LineSegment> getLineSegments(LineName lineName) {
+        ArrayList<LineSegment> result = new ArrayList<>();
+        for (LineName l : lineSegments.keySet()) {
+            if (l == lineName) {
+                result.add(new LineSegment(lineSegments.get(l).getI(), lineSegments.get(l).getII(),
+                        lineSegments.get(l).getIII(), lineSegments.get(l).getIV(), l));
+            }
+        }
+        return result;
     }
 
 }
